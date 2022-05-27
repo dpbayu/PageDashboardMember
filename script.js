116ab2e18a1da797712055cee543e218a38f16f8
Dwi Putra Bayu <74140729+dpbayu@users.noreply.github.com>
2022-05-27 15:56:34 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const image_input = document.querySelector('#image_input');
+var uploaded_image = "";
+
+image_input.addEventListener("change", function(){
+    const reader = new FileReader();
+    reader.addEventListener("load",()=>{
+        uploaded_image = reader.result;
+        document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
+        document.getElementById("#display_image").style.zIndex = "20";
+    });
+    reader.readAsDataURL(this.files[0]);
+})
